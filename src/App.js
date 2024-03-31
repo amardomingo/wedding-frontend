3a0828a3d09e4bf943fbc756ff4893b4c55296c6
Sara Jimenez <sarajcarbajal@gmail.com>
2024-03-31 22:23:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import save_date from './save_the_date.png';
+import CountdownTimer from './components/Countdown/CountdownTimer';
 import './App.css';
+import './components/Countdown/Countdown.css'
+
+const WEDDING_TIME = new Date(2024, 10, 5, 18, 0);
+const NOW_IN_MS = new Date().getTime();
+
+const date = NOW_IN_MS + WEDDING_TIME;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={save_date} className="save-date" alt="save_the_date" />
+      {/* </header> */}
+
+    <CountdownTimer targetDate={date} />
+  </div>
   );
 }
 
---
+++ src/components/Countdown/CountdownTimer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import DateTimeDisplay from './DateTimeDisplay';
+import { useCountdown } from './useCountdown';
+
+const ExpiredNotice = () => {
+  return (
+    <div className="expired-notice">
+      <span>Llegado el día es!!!</span>
+    </div>
+  );
+};
+
+const ShowCounter = ({ days, hours, minutes, seconds }) => {
+  return (
+    <div className="show-counter">
+        <DateTimeDisplay value={days} type={'días'}/>
+        <p>-</p>
+        <DateTimeDisplay value={hours} type={'horas'}/>
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={'minutos'}/>
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={'segundos'}/>
+    </div>
+  );
+};
+
+const CountdownTimer = ({ targetDate }) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+    );
+  }
+};
+
+export default CountdownTimer;
