c88b9f13edbccda8bc773a076eec70df4a0551bb
Sara Jimenez <sarajcarbajal@gmail.com>
2024-04-07 23:18:31 +0200

    added post in the backend folder

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,41 @@
 import express from 'express';
+
 import { google } from 'googleapis';
 
 const app = express()
 const PORT = process.env.PORT || 3001;
-const sheetId = process.env.SHEET_ID;
-const serviceAccountKeyFile = process.env.SERVICE_ACCOUNT_FILE;
-const auth = new google.auth.GoogleAuth({
-    keyFile: serviceAccountKeyFile,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-const authClient = await auth.getClient();
-const sheetClient = google.sheets({
-    version: 'v4',
-    auth: authClient,
-});
+
+// const sheetId = process.env.SHEET_ID;
+// const serviceAccountKeyFile = process.env.SERVICE_ACCOUNT_FILE;
+// const auth = new google.auth.GoogleAuth({
+//     keyFile: serviceAccountKeyFile,
+//     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+// });
+// const authClient = await auth.getClient();
+// const sheetClient = google.sheets({
+//     version: 'v4',
+//     auth: authClient,
+// });
+
+// middleware for exposing data on req.body
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/rsvp', async (req, res) => {
+    console.log(req.body);
+    res.write('<h1> Registration Successfull :-) </h1>');
+    res.write('<p> Name : </h1>' + req.body.name);
+    res.write('<p> Acompañantes : </h1>' + req.body.acompanante);
+    res.write('<p> Preferencias alimenticias : </h1>' + req.body.preferencias);
+    res.write('<p> Necesitas alojamiento : </h1>' + req.body.alojamiento);
+    res.write('<p> Necesitas autobús : </h1>' + req.body.autobus);
+    res.write('<p> Contacto : </h1>' + req.body.contacto);
+    res.end();
+})
 
 app.listen(PORT, () => {
     console.log(`app is running on PORT ${PORT}`)
 })
 
-app.post('/rsvp', (req, res) => {
-    const rsvp_data = req.body;
-
-    // Save the data of user that was sent by the client
-
-    // Send a response to client that will show that the request was successfull.
-    res.send({
-        message: 'Response OK',
-    });
-});
 
 export default app
+
