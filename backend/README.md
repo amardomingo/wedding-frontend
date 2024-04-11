caddb594cb8d6d97130246907ba830aa6a608275
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2024-04-11 23:45:40 +0200

    Add POC to writting on google sheets

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,5 @@
 # Backend
+
+Needs the `GOOGLE_APPLICATION_CREDENTIALS` environment
+variable pointing to a gcp service account keyfile,
+or some other google auth method.
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,8 @@
 import express from 'express';
-
-import { google } from 'googleapis';
+import insertInvitado from './sheets.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001;
-// const sheetId = process.env.SHEET_ID || '1pTcIEdOUUY_ghHs8y5CCkmZ1p0IO_9GOex3yqQ7jFA4'
-// const serviceAccountKeyFile = process.env.SERVICE_ACCOUNT_FILE || '/Users/saralberto/Downloads/my-wedding-sara-alberto-0d7859652a43.json'
-// const auth = new google.auth.GoogleAuth({
-//     keyFile: serviceAccountKeyFile,
-//     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-// });
-// const authClient = await auth.getClient();
-// const sheetClient = google.sheets({
-//     version: 'v4',
-//     auth: authClient,
-// });
 
 app.use(express.json());
 
@@ -25,7 +13,7 @@ app.get('/health', (req, res) =>{
 
 app.post('/rsvp', (req, res) => {
     console.log(req.body);
-    console.log(req.body.name)
+    insertInvitado(req.body)
     res.write("ok")
     res.end();
 })
