c793413c0d9c4bd0365b8da463231e811088b128
Sara Jiménez Carbajal <sarajcarbajal@gmail.com>
2024-06-02 18:31:13 +0200

    fix async/await function insertInvitados. Delete authClient unused variable (#4)

--- backend/sheets.js
+++ backend/sheets.js
@@ -7,13 +7,12 @@ const sheetColumns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const googleAuth = new google.auth.GoogleAuth(
     { scopes: ['https://www.googleapis.com/auth/spreadsheets']}
 )
-const authClient = googleAuth.getClient()
 const sheetClient = google.sheets({
     version: 'v4',
     auth: googleAuth,
 })
 
-export default async function insertInvitado(invitadoInfo) {
+export default function insertInvitado(invitadoInfo) {
     const data = [[
         invitadoInfo.Name,
         invitadoInfo.Apellidos,
@@ -24,7 +23,7 @@ export default async function insertInvitado(invitadoInfo) {
         invitadoInfo.Preferencias,
     ]]
     const endColumn = sheetColumns[data.length-1]
-    await sheetClient.spreadsheets.values.append({
+    sheetClient.spreadsheets.values.append({
         spreadsheetId: sheetID,
         range: `${tabName}!A:${endColumn}`,
         valueInputOption: 'USER_ENTERED',
