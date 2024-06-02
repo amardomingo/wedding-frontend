62741f295adebf6348e6d5dcecab7f41efbdd1bf
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2024-06-02 20:11:17 +0200

    Tweak the api again, implement it in the backend
    Handle the new json format for the api, and tweak it slightly
    - Remove the capitalization of the fields
    - Unify nombre and apellidos into nombre
    - Add an example for alergies
    
    Change the response so its a json containing the iban for the gifts

--- backend/sheets.js
+++ backend/sheets.js
@@ -14,14 +14,27 @@ const sheetClient = google.sheets({
 
 export default function insertInvitado(invitadoInfo) {
     const data = [[
-        invitadoInfo.Name,
-        invitadoInfo.Apellidos,
-        invitadoInfo.Contacto,
-        invitadoInfo.Acompanante,
-        invitadoInfo.Autobus,
-        invitadoInfo.Alojamiento,
-        invitadoInfo.Preferencias,
+        invitadoInfo.nombre,
+        invitadoInfo.contacto,
+        invitadoInfo.autobus,
+        invitadoInfo.alojamiento,
+        invitadoInfo.alergias == "none" ? "" : invitadoInfo.alergias,
+        "Adulto", // Menu
+        invitadoInfo.domingo,
+        "", // Acompañante de
     ]]
+    for (const acompanate of invitadoInfo.acompanantes) {
+        data.push([
+            acompanate.nombre,
+            "", //Contacto
+            invitadoInfo.autobus,
+            invitadoInfo.alojamiento,
+            acompanate.alergias == "none" ? "" : acompanate.alergias,
+            acompanate.menu,
+            invitadoInfo.domingo,
+            invitadoInfo.nombre // Acompanante de 
+        ])
+    }
     const endColumn = sheetColumns[data.length-1]
     sheetClient.spreadsheets.values.append({
         spreadsheetId: sheetID,
