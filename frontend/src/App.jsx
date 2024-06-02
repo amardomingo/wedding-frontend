6a7555bfd08f155b0e155866fdaf9dffed3a78e8
Sara Jiménez Carbajal <sarajcarbajal@gmail.com>
2024-06-02 18:13:28 +0200

    Add location to the fronted with google maps api (#2)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import RSVP from "./components/RSVP";
 import Footer from "./components/Footer";
+import Location from "./components/Location";
 
 import "./App.css";
 
@@ -14,6 +15,7 @@ const App = () => {
         <Navigation />
           <Routes>
             <Route exac path={"/rsvp"} element={<RSVP/>} />
+            <Route exac path={"/location"} element={<Location/>} />
           </Routes>
         </BrowserRouter>
       </main>
