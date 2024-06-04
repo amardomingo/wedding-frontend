29668e3a330498ffcdb415cd0be87b7df84e5818
Sara Jimenez <sarajcarbajal@gmail.com>
2024-06-04 23:15:02 +0200

    add tilde to the text and format code in the file

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div className="welcome-page">
-      <h1>¡Bienvenidos a nuestra boda!</h1>
-      <p>En esta web hemos reunido la informacion sobre la boda que vamos a celebrar</p>
-      <h2>El evento</h2>
-      <h3>Cuando</h3>
-      <p>La ceremonia comenzara a las 18:00, para luego pasar a un cocktail, la cena y el baile</p>
-      <h3>Donde</h3>
-      <p>Teneis toda la informacion sobre el lugar en el apartado de <Link to="/location">localizacion</Link></p>
-      <h3>Quienes</h3>
-      <p>Esperamos que vosotros! Por favor, rellenad vuestros datos en el apartado de <Link to="/rsvp">confirmacion de asistencia</Link> para que sepamos que venis</p>
-      <h2>Mas informacion</h2>
-      <h3>Alojamiento</h3>
-      <p>En la propia finca hay disponible alojamiento, en forma de bungalows. Hay mas informacion en <Link to="https://www.complejolaciguena.com/bungalows-madrid/">su web</Link></p>
-      <p>Si os interesa quedaros, marcadnoslo en el formulario, teneis un 10% de descuento por asistir a nuestra boda. Hemos reservado 10 bungalows que tenemos que confirmar antes de Septiembre</p>
-      <h3>Planes de domingo</h3>
-      <p>Si habeis decidido quedaros a dormir, o si os va la marcha, el restaurante de la finca ofrece la posibilidad de comer todos juntos</p>
-      <p>Si os interesa, marcad la opcion en el formulario y nos pondremos en contacto con vosotros con los detalles</p>
-    </div>
-  )
+	return (
+		<div className="welcome-page">
+			<h1>¡Bienvenidos a nuestra boda!</h1>
+			<p>En esta web hemos reunido la información necesaria para nuestro día.</p>
+			<h2>El evento</h2>
+			<h3>Cuándo</h3>
+			<p>La ceremonia comenzará a las 18:00, para luego pasar a un cocktail, la cena y el baile.</p>
+			<h3>Dónde</h3>
+			<p>Tenéis toda la información sobre el lugar en el apartado de <Link to="/location">localización</Link>.</p>
+			<h3>Quienes</h3>
+			<p>Esperamos que vosotros! Por favor, rellenad vuestros datos en el apartado de <Link to="/rsvp">confirmación de asistencia</Link> para que sepamos que venís.</p>
+			<h2>Más información</h2>
+			<h3>Alojamiento</h3>
+			<p>En la propia finca hay disponible alojamiento, en forma de bungalows. Hay mas información en <Link to="https://www.complejolaciguena.com/bungalows-madrid/">su web.</Link></p>
+			<p>Si os interesa quedaros, marcarlo en el formulario, tenéis un 10% de descuento por asistir a nuestra boda. Hemos reservado 10 bungalows que tenemos que confirmar antes de Septiembre.</p>
+			<h3>Planes de domingo</h3>
+			<p>Si habéis decidido quedaros a dormir, o si os va la marcha, el restaurante de la finca ofrece la posibilidad de comer todos juntos.</p>
+			<p>Si os interesa, marcad la opción en el formulario y nos pondremos en contacto con vosotros con los detalles.</p>
+		</div>
+	)
 };
 
 export default Home;
