f82fa68f3c3e1d72798d1dfd80ecfdbb1b04fd7f
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2024-06-04 23:22:09 +0200

    Add minimal home page (#8)
    Add minimal texts to the home page, almost placeholders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Location from "./components/Location";
 import Navigation from "./components/Navigation";
 import RSVP from "./components/RSVP";
-import Footer from "./components/Footer";
-import Location from "./components/Location";
 
 import "./App.css";
 
@@ -14,6 +15,7 @@ const App = () => {
         <BrowserRouter>
         <Navigation />
           <Routes>
+            <Route exac path={"/"} element={<Home/>} />
             <Route exac path={"/rsvp"} element={<RSVP/>} />
             <Route exac path={"/location"} element={<Location/>} />
           </Routes>
