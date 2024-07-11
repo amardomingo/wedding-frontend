03e0d8a0e591f726a1c94e7c2933cb86fb3ff209
Sara Jimenez <sarajcarbajal@gmail.com>
2024-07-11 22:58:30 +0200

    create workspace in package.json

---
+++ landing/eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+];
